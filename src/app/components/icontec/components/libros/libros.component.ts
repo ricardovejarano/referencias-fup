377f5f0d941f0c0658ae4d5c8cad611f9affa5ad
Juan Camilo Posso <jucapo05@gmail.com>
2018-10-28 10:06:01 -0500

    referencia de libros-icontec con autor dinamico

--- src/app/components/icontec/components/libros/libros.component.ts
+++ src/app/components/icontec/components/libros/libros.component.ts
@@ -22,12 +22,25 @@ export class LibrosIcontecComponent implements OnInit {
 
   anio = [];
 
+  nombres = [{ value: '' }];
+  apellidos = [{ value: '' }];
+
   referenciaFinal = '';
 
   constructor() { }
 
   ngOnInit() {
     this.getArray();
+  }
+
+  addAuthor() {
+    this.nombres.push({ value: '' });
+    this.apellidos.push({ value: '' });
+  }
+
+  quitAuthor(index) {
+    this.nombres.splice(index, 1);
+    this.apellidos.splice(index, 1);
   }
 
   getArray() {
@@ -39,38 +52,44 @@ export class LibrosIcontecComponent implements OnInit {
 
   addReference() {
     this.referenciaFinal = '';
-    if (this.apellido) {
-      this.referenciaFinal += this.apellido.toUpperCase() + ', ';
-    }
-    if (this.nombre) {
-      // return word[0].toUpperCase() + word.substr(1).toLowerCase();
-      this.referenciaFinal += this.nombre[0].toUpperCase() + this.nombre.substr(1).toLocaleLowerCase() + '. ';
+
+    for (let z = 0; z < this.nombres.length; z++) {
+      if (this.apellidos[z].value) {
+        if (z !== 0) {
+          this.referenciaFinal += ', ';
+        }
+        this.referenciaFinal += this.apellidos[z].value.toUpperCase() + ', ';
+      }
+      if (this.nombres[z].value) {
+        // return word[0].toUpperCase() + word.substr(1).toLowerCase();
+        this.referenciaFinal += this.nombres[z].value[0].toUpperCase() + this.nombres[z].value.substr(1).toLocaleLowerCase() + '. ';
+      }
     }
 
     if (this.titulo && this.subtitulo) {
-      this.referenciaFinal +=  this.titulo + ': ' + this.subtitulo + '. ';
+      this.referenciaFinal += this.titulo + ': ' + this.subtitulo + '. ';
     } else {
-      this.referenciaFinal +=  this.titulo + '. ';
+      this.referenciaFinal += this.titulo + '. ';
     }
 
     if (this.edicion) {
-      this.referenciaFinal +=  this.edicion + '. ';
+      this.referenciaFinal += this.edicion + '. ';
     }
 
     if (this.ciudad) {
-      this.referenciaFinal +=  this.ciudad + ': ';
+      this.referenciaFinal += this.ciudad + ': ';
     }
 
     if (this.editorial) {
-      this.referenciaFinal +=  this.editorial + ', ';
+      this.referenciaFinal += this.editorial + ', ';
     }
 
     if (this.anioPublicacion) {
-      this.referenciaFinal +=  this.anioPublicacion + '. ';
+      this.referenciaFinal += this.anioPublicacion + '. ';
     }
 
     if (this.paginas) {
-      this.referenciaFinal +=  this.paginas + 'p.';
+      this.referenciaFinal += this.paginas + 'p.';
     }
 
   }
