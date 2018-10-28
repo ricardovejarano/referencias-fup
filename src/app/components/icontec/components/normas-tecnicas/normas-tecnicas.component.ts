904804abaf4855d0525c130d6d4581c28ce24ec5
Juan Camilo Posso <jucapo05@gmail.com>
2018-10-28 12:18:41 -0500

    norma tecnica implementada

--- src/app/components/icontec/components/normas-tecnicas/normas-tecnicas.component.ts
+++ src/app/components/icontec/components/normas-tecnicas/normas-tecnicas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-normas-tecnicas',
@@ -7,9 +8,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NormasTecnicasComponent implements OnInit {
 
-  constructor() { }
+ // tslint:disable-next-line:max-line-length
+ ejemplo = 'INSTITUTO COLOMBIANO DE NORMALIZACIÓN Y CERTIFICACIÓN. Sistemas de gestión de la calidad: fundamentos y vocabulario. NTC-ISO 9001. Bogotá D.C.: El instituto, 2005. 36 p.';
+ entidad = '';
+ titulo = '';
+ codigo = '';
+ edicion = '';
+ ciudad = '';
+ editorial = '';
+ paginas = '';
+ anioPublicacion = 0;
+ userRegister = false;
+ anio = [];
 
-  ngOnInit() {
+ referenciaFinal = '';
+
+ constructor() { }
+
+ ngOnInit() {
+   this.getArray();
+ }
+
+ getArray() {
+
+   for (let x = 1940; x < 2020; x++) {
+     this.anio.push(x);
+   }
+ }
+
+ addReference() {
+   this.referenciaFinal = '';
+
+   if (this.entidad ) {
+     this.referenciaFinal += this.entidad.toUpperCase()  + '. ';
+   }
+
+   if (this.titulo ) {
+    this.referenciaFinal += this.titulo + '. ';
   }
+
+   if (this.codigo) {
+     this.referenciaFinal += this.codigo + '. ';
+   }
+
+   if (this.edicion) {
+     this.referenciaFinal += this.edicion + ', ';
+   }
+
+   if (this.ciudad) {
+     this.referenciaFinal += this.ciudad + ': ';
+   }
+
+   if (this.editorial) {
+     this.referenciaFinal += this.editorial + ', ';
+   }
+
+   if (this.anioPublicacion) {
+     this.referenciaFinal += this.anioPublicacion + '. ';
+   }
+
+   if (this.paginas) {
+     this.referenciaFinal += this.paginas + ' p.';
+   }
+
+ }
+
+ copyInputMessage(inputElement) {
+   inputElement.select();
+   document.execCommand('copy');
+   inputElement.setSelectionRange(0, 0);
+ }
+
+ resetForm(validForm?: NgForm) {
+   if (validForm != null) {
+     validForm.reset();
+   }
+ }
 
 }
