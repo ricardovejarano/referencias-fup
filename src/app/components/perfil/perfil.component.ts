4bd4867f9f1429e5b13e824b5ed3c7ac0562eee4
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2018-10-28 20:27:24 -0500

    Campos para perfil

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
+  // tslint:disable-next-line:max-line-length
+  srcImage = 'https://firebasestorage.googleapis.com/v0/b/referencias-fup.appspot.com/o/unknown.png?alt=media&token=dc0e2e6e-05a5-47c6-bd65-026c81d69186';
+  usuario: Usuario = new Usuario();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  fileChangeListener($event) {
+    const image: any = new Image();
+    const file: File = $event.target.files[0];
+  }
+
+  editProfile() {
+    console.log('Se edita perfil');
   }
 
 }
