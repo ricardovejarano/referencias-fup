800bc4b327b75a03783a66c924ee1557c49d9b97
Juan Camilo Posso <jucapo05@gmail.com>
2018-10-28 19:59:14 -0500

    libros- apa implementado

--- src/app/components/apa/components/libro/libro.component.ts
+++ src/app/components/apa/components/libro/libro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-libro',
@@ -7,9 +8,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibroApaComponent implements OnInit {
 
-  constructor() { }
+ // tslint:disable-next-line:max-line-length
+ ejemplo = 'Mankiw, N. G. (2014). Macroeconomía. Barcelona: Antoni Bosch.';
+ nombre = '';
+ apellido = '';
+ titulo = '';
+ ciudad = '';
+ editorial = '';
+ anioPublicacion = 0;
+ userRegister = false;
 
-  ngOnInit() {
+ anio = [];
+
+ nombres = [{ value: '' }];
+ apellidos = [{ value: '' }];
+
+ referenciaFinal = '';
+
+ constructor() { }
+
+ ngOnInit() {
+   this.getArray();
+ }
+
+ addAuthor() {
+   this.nombres.push({ value: '' });
+   this.apellidos.push({ value: '' });
+ }
+
+ quitAuthor(index) {
+   this.nombres.splice(index, 1);
+   this.apellidos.splice(index, 1);
+ }
+
+ getArray() {
+   for (let x = 1940; x < 2020; x++) {
+     this.anio.push(x);
+   }
+ }
+
+ addReference() {
+   this.referenciaFinal = '';
+
+   for (let z = 0; z < this.nombres.length; z++) {
+    if (this.apellidos[z].value) {
+      this.referenciaFinal += this.apellidos[z].value + ', ';
+    }
+    if (this.nombres[z].value) {
+      if (z !== (this.nombres.length - 1)  && this.nombres.length !== 1) {
+        this.referenciaFinal += this.nombres[z].value[0] + this.nombres[z].value.substr(1) + ', ';
+      } else {
+        this.referenciaFinal += this.nombres[z].value[0] + this.nombres[z].value.substr(1) + '. ';
+      }
+    }
   }
+
+   if (this.anioPublicacion) {
+    this.referenciaFinal += '(' + this.anioPublicacion + '). ';
+  }
+
+   if (this.titulo ) {
+     this.referenciaFinal += this.titulo + '. ' ;
+   }
+
+   if (this.ciudad) {
+     this.referenciaFinal += this.ciudad + ': ';
+   }
+
+   if (this.editorial) {
+     this.referenciaFinal += this.editorial + '.';
+   }
+
+ }
+
+ copyInputMessage(inputElement) {
+   inputElement.select();
+   document.execCommand('copy');
+   inputElement.setSelectionRange(0, 0);
+ }
+
+ resetForm(validForm?: NgForm) {
+   if (validForm != null) {
+     validForm.reset();
+   }
+ }
 
 }
