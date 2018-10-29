ceb7168af3650ee79674dc150550628a0cc72736
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2018-10-29 17:12:12 -0500

    Todos los formularios con lógica implementada

--- src/app/components/apa/components/libro/libro.component.ts
+++ src/app/components/apa/components/libro/libro.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Usuario } from 'src/app/models/usuario.model';
+import { Referencia } from 'src/app/models/referencia.model';
+import { PerfilService } from 'src/app/services/perfil.service';
+import { RankingService } from 'src/app/services/ranking.service';
 
 @Component({
   selector: 'app-libro',
@@ -8,89 +12,260 @@ import { NgForm } from '@angular/forms';
 })
 export class LibroApaComponent implements OnInit {
 
- // tslint:disable-next-line:max-line-length
- ejemplo = 'Mankiw, N. G. (2014). Macroeconomía. Barcelona: Antoni Bosch.';
- nombre = '';
- apellido = '';
- titulo = '';
- ciudad = '';
- editorial = '';
- anioPublicacion = 0;
- userRegister = false;
+  // tslint:disable-next-line:max-line-length
+  ejemplo = 'Mankiw, N. G. (2014). Macroeconomía. Barcelona: Antoni Bosch.';
+  nombre = '';
+  apellido = '';
+  titulo = '';
+  ciudad = '';
+  editorial = '';
+  anioPublicacion = 0;
+  userRegister = false;
 
- anio = [];
+  anio = [];
 
- nombres = [{ value: '' }];
- apellidos = [{ value: '' }];
+  nombres = [{ value: '' }];
+  apellidos = [{ value: '' }];
 
- referenciaFinal = '';
+  referenciaFinal = '';
+  keyAdmin = '';
+  rolUsuario = '';
+  usuarios: Usuario[];
+  referencia: Referencia = new Referencia();
+  programa = '';
+  contadorPrograma = 0;
+  contadorPersona = 0;
+  contadorReferencia = 0;
 
- constructor() { }
+  constructor(public profileService: PerfilService, public rankingService: RankingService) {
+    this.keyAdmin = localStorage.getItem('uid');
+  }
 
- ngOnInit() {
-   this.getArray();
- }
-
- addAuthor() {
-   this.nombres.push({ value: '' });
-   this.apellidos.push({ value: '' });
- }
-
- quitAuthor(index) {
-   this.nombres.splice(index, 1);
-   this.apellidos.splice(index, 1);
- }
-
- getArray() {
-   for (let x = 1940; x < 2020; x++) {
-     this.anio.push(x);
-   }
- }
-
- addReference() {
-   this.referenciaFinal = '';
-
-   for (let z = 0; z < this.nombres.length; z++) {
-    if (this.apellidos[z].value) {
-      this.referenciaFinal += this.apellidos[z].value + ', ';
+  ngOnInit() {
+    this.getArray();
+    this.getCounterReference();
+    if (localStorage.getItem('logged') === 'true') {
+      this.userRegister = true;
+      this.getRol();
+    } else {
+      console.log('NO ENTRA');
     }
-    if (this.nombres[z].value) {
-      if (z !== (this.nombres.length - 1)  && this.nombres.length !== 1) {
-        this.referenciaFinal += this.nombres[z].value[0] + this.nombres[z].value.substr(1) + ', ';
-      } else {
-        this.referenciaFinal += this.nombres[z].value[0] + this.nombres[z].value.substr(1) + '. ';
+  }
+
+  getRol() {
+    this.profileService.getRol()
+      .snapshotChanges().subscribe(item => {
+        item.forEach(element => {
+          const x = element.payload.toJSON();
+          if (element.key === this.keyAdmin) {
+            this.rolUsuario = x.toString();
+            if (this.rolUsuario !== 'administrativo') {
+              console.log('ROL', this.rolUsuario);
+              this.getProgram();
+              this.getCounterPerson();
+            }
+          }
+        });
+      });
+  }
+
+  getProgram() {
+    switch (this.rolUsuario) {
+      case 'estudiante':
+        this.profileService.getEstudiantes()
+          .snapshotChanges().subscribe(item => {
+            this.usuarios = [];
+            item.forEach(element => {
+              const x = element.payload.toJSON();
+              if (element.key === this.keyAdmin) {
+                x['$key'] = element.key;
+                if (x['programa']) {
+                  this.programa = x['programa'];
+                }
+              }
+            });
+            console.log('Programa', this.programa);
+            this.getValueConunterProgram();
+          });
+        break;
+      case 'docente':
+        this.profileService.getDocentes()
+          .snapshotChanges().subscribe(item => {
+            this.usuarios = [];
+            item.forEach(element => {
+              const x = element.payload.toJSON();
+              x['$key'] = element.key;
+              if (x['programa']) {
+                this.programa = x['programa'];
+              }
+            });
+            console.log('Programa', this.programa);
+            this.getValueConunterProgram();
+          });
+        break;
+      case 'egresado':
+        this.profileService.getEgresado()
+          .snapshotChanges().subscribe(item => {
+            this.usuarios = [];
+            item.forEach(element => {
+              const x = element.payload.toJSON();
+              x['$key'] = element.key;
+              if (x['programa']) {
+                this.programa = x['programa'];
+              }
+            });
+            console.log('Programa', this.programa);
+            this.getValueConunterProgram();
+          });
+        break;
+    }
+  }
+
+  getValueConunterProgram() {
+    if (localStorage.getItem('logged') === 'true') {
+      console.log('ÉNTRA!!!');
+      this.profileService.getContadorProgramas()
+        .snapshotChanges().subscribe(item => {
+          item.forEach(element => {
+            const x = element.payload.toJSON();
+            if (element.key === this.programa) {
+              this.contadorPrograma = Number(x['contadorActualizado']);
+              console.log('VALOR', this.contadorPrograma);
+            }
+          });
+        });
+    }
+  }
+
+  getCounterPerson() {
+    this.rankingService.getContadorPersona(this.keyAdmin, this.rolUsuario)
+      .snapshotChanges().subscribe(item => {
+        item.forEach(element => {
+          const x = element.payload.toJSON();
+          if (element.key === 'contador') {
+            this.contadorPersona = Number(x);
+          }
+        });
+      });
+  }
+
+  getCounterReference() {
+    this.rankingService.getContadorReferencia('apa', 'libro')
+      .snapshotChanges().subscribe(item => {
+        item.forEach(element => {
+          const x = element.payload.toJSON();
+          this.contadorReferencia = Number(x);
+        });
+      });
+  }
+
+  addCountProgram() {
+
+    this.rankingService.addCounterProgram(this.programa, this.contadorPrograma)
+      .then(res => {
+        console.log(res);
+      }, err => {
+        console.log('Error', err);
+      });
+  }
+
+  addCountPerson() {
+    this.rankingService.addCounterPerson(this.rolUsuario, this.keyAdmin, this.contadorPersona)
+      .then(res => {
+        console.log(res);
+      }, err => {
+        console.log('Error', err);
+      });
+  }
+
+  addCounterReference() {
+    this.rankingService.addCounterReference('apa', 'libro', this.contadorReferencia)
+      .then(res => {
+        console.log(res);
+      }, err => {
+        console.log('Error', err);
+      });
+  }
+
+  saveHistory() {
+    this.referencia.cita = this.referenciaFinal;
+    this.referencia.referencia = 'APA';
+    this.referencia.subReferencia = 'Libro';
+    this.rankingService.addReference(this.rolUsuario, this.keyAdmin, this.referencia)
+      .then(res => {
+        console.log(res);
+        window.alert('Cita guardada');
+      }, err => {
+        console.log('Error', err);
+      });
+  }
+
+  addAuthor() {
+    this.nombres.push({ value: '' });
+    this.apellidos.push({ value: '' });
+  }
+
+  quitAuthor(index) {
+    this.nombres.splice(index, 1);
+    this.apellidos.splice(index, 1);
+  }
+
+  getArray() {
+    for (let x = 1940; x < 2020; x++) {
+      this.anio.push(x);
+    }
+  }
+
+  addReference() {
+    this.addCounterReference();
+    if (localStorage.getItem('logged') === 'true') {
+      this.addCountProgram();
+      this.addCountPerson();
+    }
+
+    this.referenciaFinal = '';
+
+    for (let z = 0; z < this.nombres.length; z++) {
+      if (this.apellidos[z].value) {
+        this.referenciaFinal += this.apellidos[z].value + ', ';
+      }
+      if (this.nombres[z].value) {
+        if (z !== (this.nombres.length - 1) && this.nombres.length !== 1) {
+          this.referenciaFinal += this.nombres[z].value[0] + this.nombres[z].value.substr(1) + ', ';
+        } else {
+          this.referenciaFinal += this.nombres[z].value[0] + this.nombres[z].value.substr(1) + '. ';
+        }
       }
     }
+
+    if (this.anioPublicacion) {
+      this.referenciaFinal += '(' + this.anioPublicacion + '). ';
+    }
+
+    if (this.titulo) {
+      this.referenciaFinal += this.titulo + '. ';
+    }
+
+    if (this.ciudad) {
+      this.referenciaFinal += this.ciudad + ': ';
+    }
+
+    if (this.editorial) {
+      this.referenciaFinal += this.editorial + '.';
+    }
+
   }
 
-   if (this.anioPublicacion) {
-    this.referenciaFinal += '(' + this.anioPublicacion + '). ';
+  copyInputMessage(inputElement) {
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
   }
 
-   if (this.titulo ) {
-     this.referenciaFinal += this.titulo + '. ' ;
-   }
-
-   if (this.ciudad) {
-     this.referenciaFinal += this.ciudad + ': ';
-   }
-
-   if (this.editorial) {
-     this.referenciaFinal += this.editorial + '.';
-   }
-
- }
-
- copyInputMessage(inputElement) {
-   inputElement.select();
-   document.execCommand('copy');
-   inputElement.setSelectionRange(0, 0);
- }
-
- resetForm(validForm?: NgForm) {
-   if (validForm != null) {
-     validForm.reset();
-   }
- }
+  resetForm(validForm?: NgForm) {
+    if (validForm != null) {
+      validForm.reset();
+    }
+  }
 
 }
--- src/app/components/ieee/components/capitulo-libro-ieee/capitulo-libro-ieee.component.ts
+++ src/app/components/ieee/components/capitulo-libro-ieee/capitulo-libro-ieee.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Usuario } from 'src/app/models/usuario.model';
+import { Referencia } from 'src/app/models/referencia.model';
+import { PerfilService } from 'src/app/services/perfil.service';
+import { RankingService } from 'src/app/services/ranking.service';
 
 @Component({
   selector: 'app-capitulo-libro-ieee',
@@ -28,11 +32,176 @@ export class CapituloLibroIeeeComponent implements OnInit {
   apellidos = [{ value: '' }];
 
   referenciaFinal = '';
+  keyAdmin = '';
+  rolUsuario = '';
+  usuarios: Usuario[];
+  referencia: Referencia = new Referencia();
+  programa = '';
+  contadorPrograma = 0;
+  contadorPersona = 0;
+  contadorReferencia = 0;
 
-  constructor() { }
+  constructor(public profileService: PerfilService, public rankingService: RankingService) {
+    this.keyAdmin = localStorage.getItem('uid');
+  }
 
   ngOnInit() {
     this.getArray();
+    this.getCounterReference();
+    if (localStorage.getItem('logged') === 'true') {
+      this.userRegister = true;
+      this.getRol();
+    } else {
+      console.log('NO ENTRA');
+    }
+  }
+
+  getRol() {
+    this.profileService.getRol()
+      .snapshotChanges().subscribe(item => {
+        item.forEach(element => {
+          const x = element.payload.toJSON();
+          if (element.key === this.keyAdmin) {
+            this.rolUsuario = x.toString();
+            if (this.rolUsuario !== 'administrativo') {
+              console.log('ROL', this.rolUsuario);
+              this.getProgram();
+              this.getCounterPerson();
+            }
+          }
+        });
+      });
+  }
+
+  getProgram() {
+    switch (this.rolUsuario) {
+      case 'estudiante':
+        this.profileService.getEstudiantes()
+          .snapshotChanges().subscribe(item => {
+            this.usuarios = [];
+            item.forEach(element => {
+              const x = element.payload.toJSON();
+              if (element.key === this.keyAdmin) {
+                x['$key'] = element.key;
+                if (x['programa']) {
+                  this.programa = x['programa'];
+                }
+              }
+            });
+            console.log('Programa', this.programa);
+            this.getValueConunterProgram();
+          });
+        break;
+      case 'docente':
+        this.profileService.getDocentes()
+          .snapshotChanges().subscribe(item => {
+            this.usuarios = [];
+            item.forEach(element => {
+              const x = element.payload.toJSON();
+              x['$key'] = element.key;
+              if (x['programa']) {
+                this.programa = x['programa'];
+              }
+            });
+            console.log('Programa', this.programa);
+            this.getValueConunterProgram();
+          });
+        break;
+      case 'egresado':
+        this.profileService.getEgresado()
+          .snapshotChanges().subscribe(item => {
+            this.usuarios = [];
+            item.forEach(element => {
+              const x = element.payload.toJSON();
+              x['$key'] = element.key;
+              if (x['programa']) {
+                this.programa = x['programa'];
+              }
+            });
+            console.log('Programa', this.programa);
+            this.getValueConunterProgram();
+          });
+        break;
+    }
+  }
+
+  getValueConunterProgram() {
+    if (localStorage.getItem('logged') === 'true') {
+      console.log('ÉNTRA!!!');
+      this.profileService.getContadorProgramas()
+        .snapshotChanges().subscribe(item => {
+          item.forEach(element => {
+            const x = element.payload.toJSON();
+            if (element.key === this.programa) {
+              this.contadorPrograma = Number(x['contadorActualizado']);
+              console.log('VALOR', this.contadorPrograma);
+            }
+          });
+        });
+    }
+  }
+
+  getCounterPerson() {
+    this.rankingService.getContadorPersona(this.keyAdmin, this.rolUsuario)
+      .snapshotChanges().subscribe(item => {
+        item.forEach(element => {
+          const x = element.payload.toJSON();
+          if (element.key === 'contador') {
+            this.contadorPersona = Number(x);
+          }
+        });
+      });
+  }
+
+  getCounterReference() {
+    this.rankingService.getContadorReferencia('ieee', 'capitulo-libro')
+      .snapshotChanges().subscribe(item => {
+        item.forEach(element => {
+          const x = element.payload.toJSON();
+          this.contadorReferencia = Number(x);
+        });
+      });
+  }
+
+  addCountProgram() {
+
+    this.rankingService.addCounterProgram(this.programa, this.contadorPrograma)
+      .then(res => {
+        console.log(res);
+      }, err => {
+        console.log('Error', err);
+      });
+  }
+
+  addCountPerson() {
+    this.rankingService.addCounterPerson(this.rolUsuario, this.keyAdmin, this.contadorPersona)
+      .then(res => {
+        console.log(res);
+      }, err => {
+        console.log('Error', err);
+      });
+  }
+
+  addCounterReference() {
+    this.rankingService.addCounterReference('ieee', 'capitulo-libro', this.contadorReferencia)
+      .then(res => {
+        console.log(res);
+      }, err => {
+        console.log('Error', err);
+      });
+  }
+
+  saveHistory() {
+    this.referencia.cita = this.referenciaFinal;
+    this.referencia.referencia = 'IEEE';
+    this.referencia.subReferencia = 'Capítulo del libro';
+    this.rankingService.addReference(this.rolUsuario, this.keyAdmin, this.referencia)
+      .then(res => {
+        console.log(res);
+        window.alert('Cita guardada');
+      }, err => {
+        console.log('Error', err);
+      });
   }
 
   addAuthor() {
@@ -52,6 +221,11 @@ export class CapituloLibroIeeeComponent implements OnInit {
   }
 
   addReference() {
+    this.addCounterReference();
+    if (localStorage.getItem('logged') === 'true') {
+      this.addCountProgram();
+      this.addCountPerson();
+    }
     this.referenciaFinal = '';
 
     for (let z = 0; z < this.nombres.length; z++) {
@@ -59,7 +233,7 @@ export class CapituloLibroIeeeComponent implements OnInit {
         this.referenciaFinal += this.nombres[z].value.toUpperCase() + '. ';
       }
       if (this.apellidos[z].value) {
-          this.referenciaFinal += this.apellidos[z].value[0].toUpperCase() + this.apellidos[z].value.substr(1).toLocaleLowerCase() + ', ';
+        this.referenciaFinal += this.apellidos[z].value[0].toUpperCase() + this.apellidos[z].value.substr(1).toLocaleLowerCase() + ', ';
       }
     }
 
