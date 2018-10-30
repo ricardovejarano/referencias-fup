86d665b046e00e61885ce190d05c9ac9de640733
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2018-10-29 23:14:45 -0500

    Ranking con error en primera posición

--- src/app/components/ranking/components/graficas/graficas.component.ts
+++ src/app/components/ranking/components/graficas/graficas.component.ts
@@ -58,7 +58,7 @@ export class GraficasComponent implements OnInit {
 
   getValueConunterProgram() {
     if (localStorage.getItem('logged') === 'true') {
-      console.log('ÉNTRA!!!');
+      // console.log('ÉNTRA!!!');
       this.profileService.getContadorProgramas()
         .snapshotChanges().subscribe(item => {
           this.arrayProgramas = [];
@@ -69,10 +69,11 @@ export class GraficasComponent implements OnInit {
             this.arrayContadorProgramas.push(x['contadorActualizado']);
           });
           this.flag = false;
+          this.reorderCounterProgram();
           setTimeout(() => {
             this.barChartData = this.barChartDataFunct();
             this.barChartLabels = this.barChartLabelsFunct();
-            console.log(this.barChartData);
+            // console.log(this.barChartData);
             this.flag = true;
           }, 500);
         });
@@ -80,6 +81,22 @@ export class GraficasComponent implements OnInit {
   }
 
   reorderCounterProgram() {
+    let pos1 = 0;
+    let pos2 = 0;
+    let pos3 = 0;
+    for (let x = 0; x < this.arrayContadorProgramas.length; x++) {
+      if (this.arrayContadorProgramas[x] > this.arrayContadorProgramas[pos1]) {
+        pos3 = pos2;
+        pos2 = pos1;
+        pos1 = x;
+      } else if (this.arrayContadorProgramas[x] > this.arrayContadorProgramas[pos2]) {
+        pos3 = pos2;
+        pos2 = x;
+      } else if (this.arrayContadorProgramas[x] > this.arrayContadorProgramas[pos3]) {
+        pos3 = x;
+      }
+    }
+    console.log('POSICION 1 => ', pos1, 'POSICION 2 => ', pos2, 'POSICION 3 => ', pos3);
   }
 
   barChartDataFunct(): any {
