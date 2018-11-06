ef8c37d08ac9a73907874220a0f3c2a5100793d5
pablo kd <pablo.valencia@kcumen.co>
2018-11-06 04:57:22 -0500

    ActivatedRoute en sidebar, altura de formularios automatica corregida

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -9,24 +9,54 @@ import { AuthService } from 'src/app/services/auth.service';
   // tslint:disable-next-line:use-host-property-decorator
   host: { 'id': 'sideBar' }
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit, AfterViewInit, AfterViewChecked {
 
   flag = true;
   logged = 'false';
+  navUrl;
 
-  constructor(public router: Router, public authService: AuthService) {
+  constructor(public router: Router, public authService: AuthService, private activatedRoute: ActivatedRoute) {
     this.logged = localStorage.getItem('logged');
   }
 
   ngOnInit() {
+    this.activatedRoute.url
+      .subscribe(url => {
+        this.navUrl = String(url);
+      });
+  }
+
+  ngAfterViewInit() {
+    switch (this.navUrl) {
+      case 'icontec': this.activeReference('collapseOne'); break;
+      case 'apa': this.activeReference('collapseTwo'); break;
+      case 'ieee': this.activeReference('collapseThree'); break;
+      default:
+        break;
+    }
   }
 
   logout() {
-    console.log('CIERRA SESIÓN');
     this.router.navigate(['/']);
     localStorage.setItem('logged', 'false');
     this.authService.logout();
     this.logged = 'false';
   }
 
+
+  activeReference(collapseMenu) {
+      const accordeon = document.getElementById(collapseMenu);
+      accordeon.classList.add('show');
+  }
+
+  ngAfterViewChecked() {
+
+    const mainCard = document.getElementById('Reference');
+    const finalRef = document.getElementById('clipboard');
+    const formRef = document.getElementById('formReference');
+
+    const formHeight = mainCard.offsetHeight - finalRef.offsetHeight;
+    formRef.style.height = 'calc(' + String(formHeight) + 'px - 7rem)';
+    console.log(formHeight);
+  }
 }
