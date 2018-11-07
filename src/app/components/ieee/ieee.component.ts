532a4f4196780857d22f1330a09b0db868ac0e86
pablo kd <pablo.valencia@kcumen.co>
2018-11-07 14:03:58 -0500

    altura de formularios corregida

--- src/app/components/ieee/ieee.component.ts
+++ src/app/components/ieee/ieee.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-ieee',
   templateUrl: './ieee.component.html',
   styleUrls: ['./ieee.component.css']
 })
-export class IeeeComponent implements OnInit {
+export class IeeeComponent implements OnInit, AfterViewChecked {
 
+  formHeight = 0;
+  mainCard;
+  finalRef;
+  formRef;
   constructor() { }
 
   ngOnInit() {
+    this.mainCard = '';
+    this.finalRef = '';
+    this.formRef = '';
+    this.formHeight = 0;
   }
 
+  ngAfterViewChecked() {
+
+    this.mainCard = document.getElementById('Reference');
+    this.finalRef = document.getElementById('clipboard');
+    this.formRef = document.getElementById('formReference');
+
+    this.formHeight = this.mainCard.offsetHeight - this.finalRef.offsetHeight;
+    this.formRef.style.height = 'calc(' + String(this.formHeight) + 'px - 7rem)';
+    console.log('Form: ' + this.formHeight);
+  }
 }
