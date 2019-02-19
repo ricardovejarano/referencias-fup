85c44918e535808dd9661452d1602922fb1a1807
pablo kd <pablo.valencia@kcumen.co>
2019-02-19 16:15:17 -0500

    mobile menu fixed, forms height fixed

--- src/app/components/apa/apa.component.ts
+++ src/app/components/apa/apa.component.ts
@@ -6,11 +6,14 @@ import { Component, OnInit, AfterViewChecked } from '@angular/core';
   styleUrls: ['./apa.component.css']
 })
 export class ApaComponent implements OnInit, AfterViewChecked {
-  
+
+  deviceWidth = window.innerWidth;
   formHeight = 0;
   mainCard;
   finalRef;
   formRef;
+  timer:any;
+
   constructor() { }
 
   ngOnInit() {
@@ -22,12 +25,25 @@ export class ApaComponent implements OnInit, AfterViewChecked {
 
   ngAfterViewChecked() {
 
-    this.mainCard = document.getElementById('Reference');
-    this.finalRef = document.getElementById('clipboard');
-    this.formRef = document.getElementById('formReference');
+    if(this.deviceWidth < 768){
+      const menu = document.getElementById("mainMenuAccordion");
+      if(menu.classList.contains('show')){
+        this.timer = setTimeout (() => {
+          menu.classList.remove('show');         
+        }, 1200);
+      }else{
+        clearTimeout(this.timer);        
+      }
+    }
 
-    this.formHeight = this.mainCard.offsetHeight - this.finalRef.offsetHeight;
-    this.formRef.style.height = 'calc(' + String(this.formHeight) + 'px - 7rem)';
-    console.log('Form: ' + this.formHeight);
+    if(this.deviceWidth > 768){
+      this.mainCard = document.getElementById('Reference');
+      this.finalRef = document.getElementById('clipboard');
+      this.formRef = document.getElementById('formReference');
+
+      this.formHeight = this.mainCard.offsetHeight - this.finalRef.offsetHeight;
+      this.formRef.style.height = 'calc(' + String(this.formHeight) + 'px - 7rem)';
+      console.log('Form: ' + this.formHeight);
+    }
   }
 }
