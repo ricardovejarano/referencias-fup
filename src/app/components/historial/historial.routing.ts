ea0f26a07a995a3e7d07f0d86faa7c27c7ac1904
Ricardo <ricardo.vejarano.garcia@gmail.com>
2019-04-28 21:29:14 -0500

    Guard para Historial agregado

--- src/app/components/historial/historial.routing.ts
+++ src/app/components/historial/historial.routing.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HistorialComponent } from './historial.component';
+import { ProfileGuard } from 'src/app/guards/profile.guard';
 
 export const historialRoute: Routes = [
-    {path: 'historial', component: HistorialComponent}
+    {path: 'historial', component: HistorialComponent, canActivate: [ProfileGuard]}
 ];
