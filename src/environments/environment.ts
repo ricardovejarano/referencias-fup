e54e29c5f3550ff17c71f3cb263d7eb2c5d82ad4
Jucapo <jposso@vectorial.co>
2019-05-12 10:55:22 -0500

    cambio de estancia de Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyD3ThMO8ne--uH0gJXIhLf4SnVmH8A5oU4',
-    authDomain: 'referencias-fup.firebaseapp.com',
-    databaseURL: 'https://referencias-fup.firebaseio.com',
-    projectId: 'referencias-fup',
-    storageBucket: 'referencias-fup.appspot.com',
-    messagingSenderId: '915820890285'
+    apiKey: 'AIzaSyDUN0TpZEUihXV2Q7IwUM_jNE4snt_PlZ8',
+    authDomain: 'referencias-9cf08.firebaseapp.com',
+    databaseURL: 'https://referencias-9cf08.firebaseio.com',
+    projectId: 'referencias-9cf08',
+    storageBucket: 'referencias-9cf08.appspot.com',
+    messagingSenderId: '259109410811',
+    appId: '1:259109410811:web:bd9c1b85db67f4b0'
   }
 };
